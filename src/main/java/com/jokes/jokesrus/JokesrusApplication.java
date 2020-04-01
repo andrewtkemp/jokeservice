82a57eb7fcd857b23fab1cb371920262d7bd2604
andrewtkemp@gmail.com <andrewtkemp@gmail.com>
2020-03-31 20:23:34 -0500

    initial commit

---
+++ src/main/java/com/jokes/jokesrus/JokesrusApplication.java
@@ -0,0 +1,13 @@
+package com.jokes.jokesrus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokesrusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokesrusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jokes/jokesrus/JokesrusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jokes.jokesrus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesrusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
